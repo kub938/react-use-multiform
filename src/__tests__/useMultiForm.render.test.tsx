a52695f3b4ebc9f05fd959eb7719d03466dd97bb
kub938 <kub938@naver.com>
2026-01-08 20:14:12 +0900

    feat: isOpen(모달 형식)에 대한 테스트케이스 추가

--- src/__tests__/useMultiForm.render.test.tsx
+++ src/__tests__/useMultiForm.render.test.tsx
@@ -24,27 +24,42 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useMultiForm } from "../useMultiForm";
+import { useState } from "react";
 
-export type nameType = "첫번째 기능" | "두번째" | "세번째 기능" | "4";
+export type nameType = "첫번째 기능" | "두번째" | "세번째 기능" | "modal";
 // 테스트용 임시 컴포넌트
 const TestComponent = () => {
-  const names = ["첫번째 기능", "두번째", "세번째 기능"];
-  const { MultiForm, Step, handleStep, nowStep } = useMultiForm(names);
+  const [isOpen, setIsOpen] = useState(false);
+  const names: nameType[] = ["첫번째 기능", "두번째", "세번째 기능", "modal"];
+  const { MultiForm, Step, handleStep, nowStep } =
+    useMultiForm<nameType>(names);
 
+  const handleModal = () => {
+    handleStep("next");
+    setIsOpen(true);
+  };
   return (
     <div>
       <MultiForm>
         <Step name={"첫번째 기능"}>
           <div>첫번째</div>
         </Step>
+
         <Step name={"두번째"}>
           <div>두번째</div>
         </Step>
         <Step name={"세번째 기능"}>
           <div>세번째</div>
         </Step>
+
+        {isOpen && (
+          <Step name={"modal"}>
+            <div>모달</div>
+          </Step>
+        )}
       </MultiForm>
 
+      <button onClick={() => handleModal()}>열기</button>
       <button onClick={() => handleStep("before")}>이전</button>
       <button onClick={() => handleStep("next")}>다음</button>
     </div>
@@ -91,5 +106,20 @@ describe("MultiForm 컴포넌트 통합 테스트", () => {
     render(<TestComponent />);
 
     // userEvent.click("next")
+  });
+
+  it("모달이 열리고 해당 스텝에 해당되면 렌더링 되야한다 ", async () => {
+    const user = userEvent.setup();
+    render(<TestComponent />);
+
+    const openButton = screen.getByText("열기");
+    const nextButton = screen.getByText("다음");
+
+    await user.click(nextButton);
+    await user.click(nextButton);
+    await user.click(nextButton);
+    await user.click(openButton);
+
+    expect(screen.getByText("모달")).toBeDefined();
   });
 });
